27006d8947bc1cc95fcd2bcc45a94d0dfb179fb9
Gulmarzhan <ttaben.gulmarzhan@gmail.com>
2026-01-24 12:27:18 +0500

    Add basic Python string examples

---
+++ python-basics/string.py
@@ -0,0 +1,12 @@
+a = "Hello, World!"
+print(a[1]) #outputs e
+print(a[2:5]) #outputs llo
+print(a[:5]) #outputs Hello
+print(a[2:]) #outputs llo, World!
+print(a[-5:-2]) #outputs orl
+print(len(a)) #outputs 13
+for x in "banana":
+  print(x)
+txt = "The best things in life are free!"
+print("free" in txt)
+print("expensive" not in txt)
