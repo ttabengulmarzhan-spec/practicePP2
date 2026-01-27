baa001a6d0f01b225597fef92d388e0b2345bac2
Gulmarzhan <ttaben.gulmarzhan@gmail.com>
2026-01-27 08:15:33 +0500

    Add variables examples

--- python-basics/variables.py
+++ python-basics/variables.py
@@ -1,8 +1,42 @@
-x = str(3) #x is of type str
-y = int(3) #y is of typr int
-z = float(3) #z is of type float
-x = 4 #x is now of type int and equals to 4
+x = 5
+y = "John"
+x = "Sally" #x is now of type str
 print(x)
 print(y)
+print(type(x))
+print(type(y))
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+a, b, z = "Orange", "Banana", "Cherry"
+print(a)
+print(b)
 print(z)
+
+fruits = ["apple", "banana", "cherry"]
+c, v, n = fruits
+print(c)
+print(b)
+print(n)
+
+def myfunc():
+  global x
+  x = "fantastic"
+  print("Python is " + x)
+myfunc()
+
+print("Python is " + x)
 
