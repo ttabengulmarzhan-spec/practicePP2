863cb42b5178f890dd7237689d51c2c78c0708e1
Gulmarzhan <ttaben.gulmarzhan@gmail.com>
2026-02-03 12:37:48 +0500

    Add condition,loop examples

---
+++ Practice2/loops/for_break.py
@@ -0,0 +1,12 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
+  if x == "banana":
+    break
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
---
+++ Practice2/loops/while_loop.py
@@ -0,0 +1,12 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+
+a = 1
+while a < 6:
+  print(a)
+  a += 1
