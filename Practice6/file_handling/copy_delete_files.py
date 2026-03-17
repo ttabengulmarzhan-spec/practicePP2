68572e85192482abd559028484dd4fe02bf4dae1
Gulmarzhan <ttaben.gulmarzhan@gmail.com>
2026-03-17 13:46:12 +0500

    Add examples

--- Practice6/file_handling/copy_delete_files.py
+++ Practice6/file_handling/copy_delete_files.py
@@ -1,15 +1,14 @@
+import shutil
+import os
+
 with open("/Users/gulmarzantaben/tasks/Practice6/file_handling/lyrics.txt", "a") as f:
-    f.write("But nothing better than songs by Kairat Nurtas.\n")
+    f.write("\nBut nothing better than songs by Kairat Nurtas.\n")
 
 with open("/Users/gulmarzantaben/tasks/Practice6/file_handling/lyrics.txt", "r") as f:
     content = f.read()
     print(content)
 
-import shutil
+shutil.copy("/Users/gulmarzantaben/tasks/Practice6/file_handling/lyrics.txt", "/Users/gulmarzantaben/tasks/Practice6/file_handling/copylyr.txt")
 
-shutil.copy("lyrics.txt","copylyr.txt")
-
-import os
-
-if os.path.exists("copylyr.txt"):
-    os.remove("copylyr.txt")
+if os.path.exists("/Users/gulmarzantaben/tasks/Practice6/file_handling/copylyr.txt"):
+    os.remove("/Users/gulmarzantaben/tasks/Practice6/file_handling/copylyr.txt")
